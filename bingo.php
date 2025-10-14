740271cae30d2da0f0525d7728de20c820b2faf5
franciscolopez18n-ops <francisco.lopez18n@gmail.com>
2025-10-14 21:41:32 +0200

    Update bingo.php

--- bingo.php
+++ bingo.php
@@ -12,74 +12,123 @@
         <div class="grid__php">
             <div class="grid__php-div">
                 <h1>BINGO</h1>
-                <button>Jugar</button>
             </div>
 
 
             <?php
-            function dbg_mostrar_matriz($matriz, $titulo) {
-                echo "<div class='grid__php-div'>";
-                echo "<p>$titulo</p>";
-                echo "<table class='grid__php-table'>";
+                function dbg_mostrar_matriz($matriz, $titulo) {
+                    echo "<div class='grid__php-div'>";
+                    echo "<p>$titulo</p>";
+                    echo "<table class='grid__php-table'>";
 
-                for ($i = 0; $i < count($matriz); $i++) {
-                    echo "<tr>";
-                    for ($j = 0; $j < count($matriz[0]); $j++) {
-                        echo "<td class='grid__php-td'>".$matriz[$i][$j]."</td>";
+                    for ($i = 0; $i < count($matriz); $i++) {
+                        echo "<tr>";
+                        for ($j = 0; $j < count($matriz[0]); $j++) {
+                            echo "<td class='grid__php-td'>".$matriz[$i][$j]."</td>";
+                        }
+                        echo "</tr>";
                     }
-                    echo "</tr>";
+
+                    echo "</table>";
+                    echo "</div>";
                 }
 
-                echo "</table>";
-                echo "</div>";
-            }
+                function generar_participantes($num_jugadores, $num_cartones) {
+                    $participantes = [];
 
-            function generar_participantes($num_jugadores, $num_cartones) {
-                $participantes = [];
-
-                for ($i = 0; $i < $num_jugadores; $i++) {
-                    for ($j = 0; $j < $num_cartones; $j++) {
-                        $participantes[$i][$j] = generar_carton();
-                        dbg_mostrar_matriz($participantes[$i][$j], "Jugador ".($i + 1).", Cartón ".($j + 1)); 
+                    for ($i = 0; $i < $num_jugadores; $i++) {
+                        for ($j = 0; $j < $num_cartones; $j++) {
+                            $participantes[$i][$j] = generar_carton(); // Crea 3 cartones para cada jugador
+                            // Hace la jugada y tacha los numeros    
+                        }
                     }
+
+                    return $participantes;
                 }
 
-                return $participantes;
-            }
+                function generar_carton() {
+                    $repetidos = array_fill(1, 60, false);
+                    $carton = [];
+                    
+                    $vacios = 0;
+                    while($vacios < 6) {
+                        $i = rand(0, 2);
+                        $j = rand(0, 6);
 
-            function generar_carton() {
-                $repetidos = array_fill(1, 60, false);
-                $carton = [];
+                        if (!isset($carton[$i][$j])) {
+                            $carton[$i][$j] = '';
+                            $vacios++;
+                        }
+                    }
+
+                    for ($i = 0; $i < 3; $i++) {
+                        for ($j = 0; $j < 7; $j++) {
+                            if (isset($carton[$i][$j])) continue;
+                            
+                            $valor = 0;
+                            do {
+                                $valor = rand(1, 60);
+                            } while ($repetidos[$valor]);
+
+                            $repetidos[$valor] = true;
+                            $carton[$i][$j] = $valor;
+                        }
+                    }
+
+                    return $carton;
+                }
+
+                function jugada(&$participantes) {
+                    $ganador = false;
+                    $numeros_sacados = [];
                 
-                $vacios = 0;
-                while($vacios < 6) {
-                    $i = rand(0, 2);
-                    $j = rand(0, 6);
+                    while (!$ganador) {
+                        // Generar número aleatorio que no haya salido
+                        $num_bola = rand(1, 60);
+                        for ($jugador = 0; $jugador < count($participantes); $jugador++) // Jugadores
+                        {
+                            for ($carton = 0; $carton < count($participantes[$jugador]); $carton++) // Cartones
+                            {
+                                $contador = 0; // Hay 15 numeros (para cada carton se inicializa a 0)
+                                
+                                for ($fila = 0; $fila < count($participantes[$jugador][$carton]); $fila++) // Recorremos filas
+                                {
+                                    for ($col = 0; $col < count($participantes[$jugador][$carton][$fila]); $col++) // Recorremos columnas
+                                    {
+                                        $valor = $participantes[$jugador][$carton][$fila][$col];
 
-                    if (!isset($carton[$i][$j])) {
-                        $carton[$i][$j] = '';
-                        $vacios++;
+                                        // Tacha 
+                                        if ($valor == $num_bola) {
+                                            $participantes[$jugador][$carton][$fila][$col] = "<s class='tachado'>$valor</s>";
+                                        }   
+                                        // Comprueba si existe un elemento de esa clase
+                                        if (strpos($participantes[$jugador][$carton][$fila][$col], 'tachado') !== false) {
+                                            $contador++;
+                                        }                                                                                                        
+                                    }
+                                }
+                
+                                // Si el cartón tiene 15 números tachados → hay ganador
+                                if ($contador == 15) {
+                                    echo "<h2>El ganador es el jugador ".($jugador + 1)." con el cartón ".($carton + 1)."</h2>";
+                                    $ganador = true;
+                                    break 2; // salir de ambos for
+                                }
+                            }//cartones
+                        }//jugadores
+                    }
+                
+                    // Mostrar resultado final: todos los cartones con sus X
+                    foreach ($participantes as $i => $jugador) {
+                        foreach ($jugador as $j => $carton) {
+                            dbg_mostrar_matriz($carton, "Jugador ".($i + 1).", Cartón ".($j + 1));
+                        }
                     }
                 }
 
-                for ($i = 0; $i < 3; $i++) {
-                    for ($j = 0; $j < 7; $j++) {
-                        if (isset($carton[$i][$j])) continue;
-                        
-                        $valor = 0;
-                        do {
-                            $valor = rand(1, 60);
-                        } while ($repetidos[$valor]);
+                $participantes = generar_participantes(4, 3);
+                jugada($participantes);
 
-                        $repetidos[$valor] = true;
-                        $carton[$i][$j] = $valor;
-                    }
-                }
-
-                return $carton;
-            }
-
-            $participantes = generar_participantes(4, 3);
 
             ?>      
 
